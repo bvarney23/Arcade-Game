35c805b915733e1c8e4cc66c22d674bcef35f86f
Brandon Varney <b.varney03@gmail.com>
2022-10-06 08:59:55 -0400

    Updating Files

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-let gamestate = {
+let gameState = {
     gameBoard: [
-        [[], [], []],
-        [[], [], []],
-        [[], [], []]
+        [[null], [null], [null]],
+        [[null], [null], [null]],
+        [[null], [null], [null]]
     ],
     currentPlayerTurn: "",
     winningConditions: []
 }
 
+let gameBoardContainer = document.getElementById("game-board");
+
+function renderGame () {
+    for (let numOfRowsMade = 0; numOfRowsMade < gameState.gameBoard.length; numOfRowsMade++) {
+        let newRowElement = document.createElement("div");
+        newRowElement.classList.add("row");
+        let currentJSRow = gameState.gameBoard[numOfRowsMade];
+    
+        for (let numOfCellsMade = 0; numOfCellsMade < currentJSRow.length; numOfCellsMade++) {
+            let newCellElement = document.createElement("div");
+            newCellElement.classList.add("cell");
+
+            if (currentJSRow[numOfCellsMade] != null) {
+                newCellElement.textContent = currentJSRow[numOfCellsMade];
+            } else {
+                newCellElement.textContent = "Empty"
+            }
+
+            newRowElement.appendChild(newCellElement)
+        }
+    
+        gameBoardContainer.appendChild(newRowElement);
+
+    }
+};
+
+document.addEventListener("DOMContentLoaded", renderGame)
